c06f7b4cb8b12fee7aa2a86dbb276e849535524e
181G0515@rcarbonifera.tecnm.mx <181G0515@rcarbonifera.tecnm.mx>
2021-09-30 03:04:07 -0500

    Final

---
+++ Unidad1/Actividad2/Controllers/Home.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Actividad2.Models;
+
+namespace Actividad2.Controllers
+{
+    public class Home : Controller
+    {
+        public IActionResult Index(float c1, float c2, float c3)
+        {
+            PromedioViewModel promedio = new PromedioViewModel();
+            promedio.Calif1 = c1;
+            promedio.Calif2 = c2;
+            promedio.Calif3 = c3;
+
+            return View(promedio);
+        }
+    }
+}
---
+++ Unidad1/Actividad2/Models/PromedioViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Actividad2.Models
+{
+    public class PromedioViewModel
+    {
+        
+            public float Calif1 { get; set; }
+            public float Calif2 { get; set; }
+            public float Calif3 { get; set; }
+
+
+
+            public float Suma { get { return Calif1 + Calif2 + Calif3; } }
+
+
+
+            public float Promedio
+            {
+                get
+                {
+                    return (Suma / 3);
+                }
+            }
+
+
+
+            public string EstadoUsuario()
+            {
+                {
+                    if (Promedio == 0)
+                    {
+                        return "Sin calificar";
+                    }
+                    else if (Promedio >= 70)
+                    {
+                        return "Aprobado";
+                    }
+                    else
+                    {
+                        return "Reprobado";
+                    }
+                }
+            }
+        }
+    }
